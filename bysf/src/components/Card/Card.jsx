6c8415e326a1249236ea5d82ebd0c6cce25f53f3
Guilherme webber <guiz1n.webber@gmail.com>
2025-02-19 15:14:36 -0300

    styles and logics

--- bysf/src/components/Card/Card.jsx
+++ bysf/src/components/Card/Card.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Card.css";
 function Card({ name, description, image, price, type, spanType, prevPrice }) {
-
   return (
     <div className={`containerCard ${type}`}>
       <div className="divImg">
@@ -10,12 +9,27 @@ function Card({ name, description, image, price, type, spanType, prevPrice }) {
       </div>
       <div className="infosCard">
         <h4 className="name">{name}</h4>
-        <p className="description">{description}</p>
         <div className="divPrice">
           <div className="flex">
-            <span className="prevPrice">{prevPrice.toFixed(2).replace('.', ',')}</span>
-            <p className="price">R$ {price.toFixed(2).replace('.', ',')}</p>
+            <span className="prevPrice">
+              {prevPrice !== 0 && (
+                <span className="prevPrice">
+                  {prevPrice.toFixed(2).replace(".", ",")}
+                </span>
+              )}
+            </span>
+            <p className="price">R$ {price.toFixed(2).replace(".", ",")}</p>
           </div>
+          <div>
+            <p className="installments">
+              <span className="bold">3x</span> de{" "}
+              <span className="bold">
+                {(price / 3).toFixed(2).replace(".", ",")}
+              </span>{" "}
+              sem juros{" "}
+            </p>
+          </div>
+          <button className="btnAdd"> + Adicionar</button>
         </div>
       </div>
     </div>
