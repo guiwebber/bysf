20d74cd161c0adc93bf26be67314dae203d6a485
guilherme <guiz1n.webber@gmail.com>
2025-02-19 19:14:02 -0300

    search input

--- bysf/src/components/Card/Card.jsx
+++ bysf/src/components/Card/Card.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 import "./Card.css";
-import img from '../../assets/images/img1.jpg'
+import img from "../../assets/images/img1.jpg";
 
 import ButtonAdd from "../ButtonAdd/ButtonAdd";
-function Card({ name, description, image, price, type, spanType, prevPrice, onCardClick }) {
+function Card({
+  name,
+  description,
+  image,
+  price,
+  type,
+  spanType,
+  prevPrice,
+  onCardClick,
+}) {
   return (
-    <div className={`containerCard ${type}`} >
-      <div className="divImg" onClick={() => onCardClick({ name, description, image, price, type, prevPrice })}>
+    <div className={`containerCard ${type}`}>
+      <div
+        className="divImg"
+        onClick={() =>
+          onCardClick({ name, description, image, price, type, prevPrice })
+        }
+      >
         <span className={`${spanType}`}>{type}</span>
         <img className="img" src={img} alt={name} />
       </div>
       <div className="infosCard">
-        <h4 className="name" onClick={() => onCardClick({ name, description, image, price, type, prevPrice })}>{name}</h4>
+        <h4
+          className="name"
+          onClick={() =>
+            onCardClick({ name, description, image, price, type, prevPrice })
+          }
+        >
+          {name}
+        </h4>
         <div className="divPrice">
           <div className="flex">
             <span className="prevPrice">
@@ -24,7 +45,8 @@ function Card({ name, description, image, price, type, spanType, prevPrice, onCa
             </span>
             <p className="price">R$ {price.toFixed(2).replace(".", ",")}</p>
           </div>
-          <ButtonAdd/>
+            <p className="installments">ou 3x de {(price / 3).toFixed(2).replace(".", ",")}</p>
+          <ButtonAdd />
         </div>
       </div>
     </div>
