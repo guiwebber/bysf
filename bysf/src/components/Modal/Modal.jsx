ee01bc57dadbae56ee1f3f7679cfe78b8d41150e
Guilherme webber <guiz1n.webber@gmail.com>
2025-02-21 10:32:02 -0300

    bugs

--- bysf/src/components/Modal/Modal.jsx
+++ bysf/src/components/Modal/Modal.jsx
@@ -7,19 +7,28 @@ function Modal({ isOpen, onClose, product }) {
   return (
     <div className="modalOverlay" onClick={onClose}>
       <div className="modalContent" onClick={(e) => e.stopPropagation()}>
-        <div>
+        <button>left</button>
+        <div className="divContent">
           <button className="closeBtn" onClick={onClose}>
             X
           </button>
           <h2>{product.name}</h2>
           <img className="imgModal" src={product.image} alt={product.name} />
           <p>{product.description}</p>
-          <p>R$ {product.price.toFixed(2).replace(".", ",")}</p>
-          {product.prevPrice !== 0 && (
-            <span>De: R$ {product.prevPrice.toFixed(2).replace(".", ",")}</span>
-          )}
+          <div>
+            {product.prevPrice !== 0 && (
+              <span className="promo">
+                R$ {product.prevPrice.toFixed(2).replace(".", ",")}
+              </span>
+            )}
+            <p>R$ {product.price.toFixed(2).replace(".", ",")}</p>
+          </div>
+          <ButtonAdd />
         </div>
-        <ButtonAdd />
+        <button>right</button>
+      </div>
+      <div className="otherImgs">
+        <p>other imgs</p>
       </div>
     </div>
   );
